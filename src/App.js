f03bdac39355be9d12bb1581630e24f006f5a09e
Saleh-123 <67531889+Saleh-123@users.noreply.github.com>
2020-08-22 01:25:21 +0600

    working with React

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
+  const product = [
+    {name:'Photoshop', price:'$90.99'},
+    {name:'Illustrator', price:'$60.99'},
+    {name:'PDF Reader', price:'$6.99'}
+  ]
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>I am a Raect Person</p>
+       <Product product={product[0]}></Product>
+       <Product product={product[1]}></Product>
+      
       </header>
     </div>
   );
+}
+
+function Product(props){
+  const productStyle={
+    border:'ipx solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  
+
+  return(
+    <div style={productStyle}>
+        <h3>{props.product.name}</h3>
+        <h5>{props.product.price}</h5>
+        <button>Buy now</button>
+    </div>
+  )
 }
 
 export default App;
